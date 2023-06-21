44991767df636cb081d62a6a6a44b6c1c1e7ac52
ICanSeeTheHalo <201844087@itc.ac.kr>
2023-06-22 00:39:19 +0900

    Initial commit

---
+++ app/src/main/java/com/moonrabbit/ghost/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.moonrabbit.ghost
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
